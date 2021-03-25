5ccce33cd34f46bc33eb9677b0a47f6156cae8c7
Francis Deh <dfdemajesty699@gmail.com>
2021-03-25 16:21:18 +0000

    project scaffold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { MarketDataComponent } from './pages/market-data/market-data.component';
+import { MakeTradeComponent } from './pages/make-trade/make-trade.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoaderComponent,
+    DashboardComponent,
+    LoginComponent,
+    RegisterComponent,
+    MarketDataComponent,
+    MakeTradeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pages/make-trade/make-trade.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-make-trade',
+  templateUrl: './make-trade.component.html',
+  styleUrls: ['./make-trade.component.css']
+})
+export class MakeTradeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
